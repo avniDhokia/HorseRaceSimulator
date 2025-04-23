2c4aeef5ed991fe5931d783a8225d64a545f10f0
AvniDhokia <avDhokia@protonmail.com>
2025-04-23 22:25:41 +0100

    Updated Horse class so equipment affects race performance (eg speed or confidence)

--- Part2/Horse.java
+++ Part2/Horse.java
@@ -1,4 +1,3 @@
-import java.io.*;
 
 /**
  * Write a description of class Horse here.
@@ -12,27 +11,17 @@ public class Horse
     char horseSymbol;
     String horseName;
     double horseConfidence;
-    double distanceTravelled;	// arbitrary units
+    int distanceTravelled;	// arbitrary units
     boolean horseFallen;	// true if fallen
-
-    // additional attributes for GUI implementation
-    Breed breed;
-    String colour;
-    String saddle;
-    String horseshoes;
-    String bridle;
-    String blankets;
-    String hats;
-
-    double odds;
-
+    
       
-    //Constructors of class Horse
-
-    public Horse(char horseSymbol, String horseName, double horseConfidence, String colour, Breed breed){
+    //Constructor of class Horse
+    /**
+     * Constructor for objects of class Horse
+     */
+    public Horse(char horseSymbol, String horseName, double horseConfidence)
+    {
         this.horseSymbol = horseSymbol;
-        this.breed = breed;
-        this.colour = colour;
         
         // ensure horseName is valid
         if (horseName == null || horseName.length() == 0){
@@ -57,87 +46,60 @@ public class Horse
 
         return;
     }
-
-    // read a Horse from a file, given a String read from a CSV (formatted in a specific way)
-    public Horse(String line) throws IOException{
-
-        /* Structure of one line of Horse file:
-            name,confidence,representation,colour,breedName,breedSpeed,breedStamina
-        */
-
-        if (line == null){
-            System.out.println("Error: Horse file is empty");
-            return;
-        }
-
-        String[] brokenLine = line.split(",");
-
-        horseName = brokenLine[0];
-        horseConfidence = Double.parseDouble( brokenLine[1] );
-        horseSymbol = brokenLine[2].charAt(0);
-        colour = brokenLine[3];
-        breed = new Breed(brokenLine[4], Double.parseDouble(brokenLine[5]), Double.parseDouble(brokenLine[6]));
-
-        return;
-    }
-
-    // write the Horse to a file, given a PrintWriter
-    public void storeHorseCSV(PrintWriter pw){
-
-        /* Structure of one line of Horse file:
-            name,confidence,representation,colour,breedName,breedSpeed,breedStamina
-        */
-       
-        pw.println(horseName + "," + horseConfidence + "," + horseSymbol + "," + colour + "," + breed.getBreedStringCSV());
-        return;
-    }
-
-    // return a copy of the horse
-    public Horse copyHorse(){
-        return new Horse(horseSymbol, horseName, horseConfidence, colour, breed);
-    }
-
-
+    
+    
+    
     //Other methods of class Horse
-    public void fall(){
+    public void fall()
+    {
+        horseConfidence = horseConfidence - 0.1;
+        
+       	if (horseConfidence <= 0){
+            horseConfidence = 0.1;
+        }
+        
         horseFallen = true;
     }
     
-    public double getConfidence(){
+    public double getConfidence()
+    {
         return horseConfidence;
     }
     
-    public double getDistanceTravelled(){
+    public int getDistanceTravelled()
+    {
         return distanceTravelled;
     }
     
-    public String getName(){
+    public String getName()
+    {
         return horseName;
     }
     
-    public char getSymbol(){
+    public char getSymbol()
+    {
         return horseSymbol;
     }
-
-    public Breed getBreed(){
-        return breed;
-    }
     
-    public void goBackToStart(){
+    public void goBackToStart()
+    {
         distanceTravelled = 0;
         return;
     }
     
-    public boolean hasFallen(){
+    public boolean hasFallen()
+    {
         return horseFallen;
     }
 
-    public void moveForward(){
+    public void moveForward()
+    {
         distanceTravelled = distanceTravelled + 1;
         return;
     }
 
-    public void setConfidence(double newConfidence){
+    public void setConfidence(double newConfidence)
+    {
     	if (newConfidence > 1 || newConfidence < 0){
     		System.out.println("Error: Confidence not in range: " + newConfidence);
     	}
@@ -147,23 +109,10 @@ public class Horse
         return;
     }
     
-    public void setSymbol(char newSymbol){
+    public void setSymbol(char newSymbol)
+    {
         this.horseSymbol = newSymbol;
         return;
     }
-
-    public void setName(String newName){
-        this.horseName = newName;
-        
-        return;
-    }
-
-    public void setOdds(double odds){
-        this.odds = odds;
-        return;
-    }
-
-    public double getOdds(){
-        return odds;
-    }
+    
 }
