b07b86cb3b9c4f2a789e9cd6d79329f50dd0d196
AvniDhokia <avDhokia@protonmail.com>
2025-04-23 22:24:33 +0100

    Updated BettingAccount class to better implement Bet objects

--- Part2/BettingAccount.java
+++ Part2/BettingAccount.java
@@ -1,6 +1,6 @@
 class BettingAccount{
     int apples;         // apples is the currency of the betting system
-    int betAmount;
+    Bet bet = new Bet(0, null);
 
     public BettingAccount(int startApples){
         apples = startApples;
@@ -12,13 +12,13 @@ class BettingAccount{
 
     // set the current betting amount to the given value
     // do not allow if the betAmount is greater than the user's apples
-    public void makeBet(int betAmount){
+    public void makeBet(int betAmount, Horse betHorse){
         if (apples > betAmount){
-            this.betAmount = betAmount;
+            bet = new Bet(betAmount, betHorse);
         }
         else if (apples == betAmount){
             System.out.println("If User loses this bet, they will have no apples remaining");
-            this.betAmount = betAmount;
+            bet = new Bet(betAmount, betHorse);
         }
         else{
             System.out.println("User does not have enough apples to make this bet");
@@ -27,23 +27,35 @@ class BettingAccount{
         return;
     }
 
+    // return the Bet object
+    public Bet getBet(){
+        return bet;
+    }
+
     // return the current betting amount
-    public int getBet(){
-        return betAmount;
+    public int getBetAmount(){
+        return bet.getAmount();
+    }
+
+    // return the horse that the user is betting on
+    public Horse getBetHorse(){
+        return bet.getHorse();
     }
 
     // win the bet, and set betAmount to 0
     public void winBet(){
-        apples = apples + betAmount;
-        betAmount = 0;
+        apples = apples + (bet.getAmount() * (int)bet.getHorse().getOdds());
+        bet.setAmount(0);
+        bet.setHorse(null);
         return;
     }
 
     // lose the bet, and set betAmount to 0
     // if the user loses more apples than they have, warn them and set their apples to 0
     public void loseBet(){
-        apples = apples - betAmount;
-        betAmount = 0;
+        apples = apples - bet.getAmount();
+        bet.setAmount(0);
+        bet.setHorse(null);
 
         if (apples <= 0){
             System.out.println("Warning: User has lost all their apples");
@@ -55,14 +67,14 @@ class BettingAccount{
 
     // increase the bet amount by the given value
     public void increaseBet(int increaseBy){
-        int tempBet = betAmount + increaseBy;
+        int tempBet = bet.getAmount() + increaseBy;
 
         if (apples > tempBet){
-            betAmount = tempBet;
+            bet.setAmount(tempBet);
         }
         else if (apples == tempBet){
             System.out.println("If User loses this bet, they will have no apples remaining");
-            betAmount = tempBet;
+            bet.setAmount(tempBet);
         }
         else{
             System.out.println("User does not have enough apples to make this bet");
@@ -73,13 +85,12 @@ class BettingAccount{
 
     // decrease the bet amount by the given value
     public void decreaseBet(int decreaseBy){
-
-        if (betAmount < decreaseBy){
+        if (bet.getAmount() < decreaseBy){
             System.out.println("User cannot decrease bet amount by more than current bet amount");
             return;
         }
 
-        betAmount = betAmount - decreaseBy;
+        bet.setAmount(bet.getAmount() - decreaseBy);
         return;
     }
 }
